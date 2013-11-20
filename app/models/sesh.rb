f0649deecfea722857cb3766581bbbdf0d855826
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-11-20 13:35:43 -0500

    Made sesh class in model

--- app/models/sesh.rb
+++ app/models/sesh.rb
@@ -1,2 +1,33 @@
 class Sesh < ActiveRecord::Base
+
+	@@all = []
+
+	def initialize
+		@name = name
+		@created_at = created_at
+		@ended_at=ended_at
+	end
+	
+	def name
+		@name
+	end
+
+	def created_at
+		@created_at
+	end
+
+	def ended_at
+		@ended_at
+	end
+
+	def self.create(name, created_at, ended_at)
+		sesh = Sesh.new(name, created_at, ended_at)
+		@@all << sesh
+		sesh
+	end	
+
+	def self.all
+		@@all
+	end
+
 end
