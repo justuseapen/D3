2b4a0d146e334ded4ad0a5e0787db481837a0524
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-11-20 13:53:46 -0500

    fixed class and instance errors and session form

--- app/models/sesh.rb
+++ app/models/sesh.rb
@@ -1,37 +1,7 @@
 class Sesh < ActiveRecord::Base
 
-	@@all = []
-
-	def initialize
-		@name = name
-		@created_at = created_at
-		@ended_at=ended_at
-	end
-	
-	def name
-		@name
-	end
-
-	def created_at
-		@created_at
-	end
-
-	def ended_at
-		@ended_at
-	end
-
-	def self.length
-		@sesh_length = @ended_at - @created_at
-	end
-
-	def self.create(name, created_at, ended_at)
-		sesh = Sesh.new(name, created_at, ended_at)
-		@@all << sesh
-		sesh
-	end	
-
-	def self.all
-		@@all
+	def length
+	 ended_at - created_at
 	end
 
 end
