d571ceacde4a67fa2c9343aafe5046df1e71e2d1
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-11-20 17:23:34 -0500

    Formatted time and applied duration to session index partial

--- app/models/sesh.rb
+++ app/models/sesh.rb
@@ -1,7 +1,14 @@
 class Sesh < ActiveRecord::Base
 
 	def length
-	 ended_at - created_at
+	 in_seconds = ended_at - created_at
+	 in_minutes = in_seconds/60
+	 rounded = in_minutes.round
+	 if rounded == 1
+	 	shows = "#{rounded} minute"
+	 else
+	 shows = "#{rounded} minutes"
+	 end
 	end
 
 end
