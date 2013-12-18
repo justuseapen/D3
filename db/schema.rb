313aa40891485b8ddd7b349c462e1ac47e796270
Justus Danny Eapen Jr <justuseapen@gmail.com>
2013-12-18 10:29:43 -0500

    Successful move to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131123180217) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "seshes", force: true do |t|
     t.string   "name"
     t.datetime "ended_at"
@@ -21,12 +24,7 @@ ActiveRecord::Schema.define(version: 20131123180217) do
     t.integer  "user_id"
   end
 
-  add_index "seshes", ["user_id"], name: "index_seshes_on_user_id"
-
-  create_table "tracs", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  add_index "seshes", ["user_id"], name: "index_seshes_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -43,7 +41,7 @@ ActiveRecord::Schema.define(version: 20131123180217) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
